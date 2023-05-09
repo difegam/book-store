d4d6f040940421359e0f25f61850962a8f562751
difegam <difegam@gmail.com>
2023-05-09 15:20:21 +0200

    add aggregation and ordering

--- book_outlet/views.py
+++ book_outlet/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Avg, Max, Min
 from django.shortcuts import get_object_or_404, render
 
 from .models import Book
@@ -5,16 +6,37 @@ from .models import Book
 
 # Create your views here.
 def index(request):
-    books = Book.objects.all()
-    return render(request, "book_outlet/index.html", context={"books": books})
+    books = Book.objects.all().order_by("title")  # ASC
+    # books = Book.objects.all().order_by("-title") # DESC
+
+    num_books = books.count()
+    rating_metrics = books.aggregate(Avg("rating"), Min("rating"), Max("rating"))
+
+    context = {
+        "books": books,
+        "total_number_of_books": num_books,
+        "rating_metrics": rating_metrics,
+    }
+
+    return render(request, "book_outlet/index.html", context=context)
 
 
 def book_detail(request, slug: str):
+
+    # * Option 1
     # try:
     #     book = Book.objects.get(pk=id)
     # except ObjectDoesNotExist as e:
     #     raise Http404("Book doesn't exist") from e
 
+    # * Option 2
     book = get_object_or_404(Book, slug=slug)
-    context = {"title": book.title, "author": book.author, "rating": book.rating, "is_bestselling": book.is_bestselling}
+
+    context = {
+        "title": book.title,
+        "author": book.author,
+        "rating": book.rating,
+        "is_bestselling": book.is_bestselling,
+    }
+
     return render(request, "book_outlet/book_detail.html", context=context)
