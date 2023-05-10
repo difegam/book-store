259e6a1d9a5655b04ddfc5cb7b3c6519d5e94cc1
difegam <difegam@gmail.com>
2023-05-10 12:47:44 +0200

    add admin config

--- book_outlet/admin.py
+++ book_outlet/admin.py
@@ -6,6 +6,8 @@ from .models import Book
 # Register your models here.
 class BookAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title",)}
+    list_filter = ("author", "rating")
+    list_display = ("title", "author")
 
 
 admin.site.register(Book, BookAdmin)
